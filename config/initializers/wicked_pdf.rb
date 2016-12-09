29a6f31084e3ee4e09984f4ae50b4717a5293b97
RakshithHebbarVS <rakshithhebbar123@gmail.com>
2016-12-09 20:04:12 +0530

    Added gems

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
-wkhtmltopdf_path = “#{Rails.root}/bin/wkhtmltopdf-amd64”
+wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
 else
-wkhtmltopdf_path = “/usr/local/bin/wkhtmltopdf”
+wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"
 end
 WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path}
